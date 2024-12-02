a5352947741255ff9f56dc00976fc2b73d9079dc
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-12-02 10:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
 function insert_Row() {
-    //Write your code here
-  
+	const table = document.getElementById('sampleTable');
+
+	  const row1 = table.insertRow(0);
+
+	const cell1=row1.insertCell(0);
+	const cell2 = row1.insertCell(1);
+
+	cell1.textContent="New Cell1";
+	cell2.textContent="New Cell2";
+
   
 }
+
+
+
